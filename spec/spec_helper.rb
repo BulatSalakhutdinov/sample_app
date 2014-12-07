efbfc7ef6245c3cc8333b33a155af7475d1a0c86
BulatSalakhutdinov <vaiiio@yandex.ru>
2014-12-08 01:00:59 +0400

    Testing the title and passing the test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -82,4 +82,9 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+  #config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  #config.use_transactional_fixtures = true
+  #config.infer_base_class_for_anonymous_controllers = false
+  #config.order = "random" 
+  #config.include Capybara::DSL
 end
